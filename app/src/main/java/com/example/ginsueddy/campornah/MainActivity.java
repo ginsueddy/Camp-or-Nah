b47e171bb624a25cd7099d5238e2d8c6b233b474
ginsueddy <ginsu.eddy@gmail.com>
2018-01-12 11:38:13 -0800

    Got Google Play Services Key and completed Google Play Serveces check

--- app/src/main/java/com/example/ginsueddy/campornah/MainActivity.java
+++ app/src/main/java/com/example/ginsueddy/campornah/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.ginsueddy.campornah;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServiceOK: checking google services value");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //success
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error has occurred but it can be fixed
+            Log.d(TAG, "isServicesOK: an error occured but is fixable");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
