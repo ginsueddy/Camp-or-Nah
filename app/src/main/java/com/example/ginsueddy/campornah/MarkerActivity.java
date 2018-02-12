0c5600159002fedd3402d3a251af2892a11eb580
Eddy <EddyGin18@issaquah.wednet.edu>
2018-02-12 09:54:58 -0800

    begining to set methods and variables for MarkerActivity

--- app/src/main/java/com/example/ginsueddy/campornah/MarkerActivity.java
+++ app/src/main/java/com/example/ginsueddy/campornah/MarkerActivity.java
@@ -1,11 +1,13 @@
 package com.example.ginsueddy.campornah;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,8 +19,11 @@ public class MarkerActivity extends AppCompatActivity {
 
     private static final String TAG = "MarkerActivity";
 
-    private TextView mLatitudeDisplay;
-    private TextView mLongitudeDisplay;
+    private EditText mNameEditText;
+    private EditText mDescriptionEditText;
+    private TextView mLatitudeTextView;
+    private TextView mLongitudeTextView;
+    private ImageButton mFinishImageButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,18 +31,36 @@ public class MarkerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_marker);
         Toast.makeText(this, "Intent success", Toast.LENGTH_SHORT).show();
 
-        mLatitudeDisplay = (TextView) findViewById(R.id.latitude_text_view);
-        mLongitudeDisplay = (TextView) findViewById(R.id.longitude_text_view);
+        mNameEditText = (EditText) findViewById(R.id.name_edit_text);
+        mDescriptionEditText = (EditText) findViewById(R.id.description_edit_text);
+        mLatitudeTextView = (TextView) findViewById(R.id.latitude_text_view);
+        mLongitudeTextView = (TextView) findViewById(R.id.longitude_text_view);
+        mFinishImageButton = (ImageButton) findViewById(R.id.ic_finish_task_image_button);
 
         Intent intentPackage = getIntent();
-        Bundle extras = intentPackage.getExtras();
+        Bundle intentPackageExtras = intentPackage.getExtras();
         double latitude = 0;
-        double longitude;
+        double longitude = 0;
         if(intentPackage != null){
-            latitude = extras.getDouble("EXTRA_LATITUDE");
+            latitude = intentPackageExtras.getDouble("EXTRA_LATITUDE");
         }
 
-        Toast.makeText(this, "latitude: " + latitude, Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onCreate: mylatitude " + latitude);
+
+        mLatitudeTextView.setText((int) latitude);
+        mLongitudeTextView.setText((int) longitude);
+        //set hella if statements for completeMarker
     }
+
+    private void completeMarker(){
+        mFinishImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentToMapActivity = new Intent();
+                Bundle intentToMapActivityExtras = new Bundle();
+                //extras.put()
+            }
+        });
+    }
+
 }
