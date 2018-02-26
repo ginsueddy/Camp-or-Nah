2dd4306bbd5039078ebbb0c9f44c5d8a69d01005
ginsueddy <ginsu.eddy@gmail.com>
2018-02-25 16:43:51 -0800

    Completed layout for detail activity

---
+++ app/src/main/java/com/example/ginsueddy/campornah/camp/spot/list/CampSpotDetailActivity.java
@@ -0,0 +1,8 @@
+package com.example.ginsueddy.campornah.camp.spot.list;
+
+/**
+ * Created by ginsueddy on 2/25/18.
+ */
+
+public class CampSpotDetailActivity {
+}
--- app/src/main/java/com/example/ginsueddy/campornah/camp/spot/list/CampSpotListActivity.java
+++ app/src/main/java/com/example/ginsueddy/campornah/camp/spot/list/CampSpotListActivity.java
@@ -1,5 +1,6 @@
 package com.example.ginsueddy.campornah.camp.spot.list;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
